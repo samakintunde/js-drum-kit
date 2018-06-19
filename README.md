1a30bb09589bdf3635602689c6994d0288b7e8f5
Samuel Akintunde <samakintunde37@gmail.com>
2018-06-19 03:57:37 +0100

    Restyled entire app

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # js-drum-kit
+
 A Javascript app that plays drum beats on key press.
 ###From Wes Bos' Javascript30 course.
+
+Icon: Drum by ATOM from the Noun Project
--- main.js
+++ main.js
@@ -1,18 +1,18 @@
+window.addEventListener("keydown", playAudio)
+
 function playAudio(e) {
   // To instantiate audio
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-
+  
   if (!audio) return;
   audio.play();
   audio.currentTime = 0;
-
+  
   // To animate key clicked
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
   key.classList.add("key-playing");
 }
-
-window.addEventListener("keydown", playAudio);
 
 function removeTransition(e) {
   if (e.propertyName !== "transform") return;
